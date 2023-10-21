7eb43c3b5bea9fb6d3abd70f0a92577b0c8884ea
Vijay Varadarajan <vveejaay11@yahoo.com>
2023-10-21 12:30:14 +0530

    changes made to layout

--- project/settings.py
+++ project/settings.py
@@ -149,8 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "vedassist/static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- vedassist/views.py
+++ vedassist/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from django.core.mail import EmailMessage
 
 # Create your views here.
+def index(request):
+    return render(request, "vedassist/index.html")
