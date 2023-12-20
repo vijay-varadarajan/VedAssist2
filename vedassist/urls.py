2748919f3bfbb2cf3202fe798f4f711b7ee0b460
Amrit Sundarka <amritsundarka@gmail.com>
2023-12-20 13:01:34 +0530

    Linked the pages together

--- vedassist/urls.py
+++ vedassist/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name='index'),
     path("login/", views.login, name='login'),
-    path("signup/", views.signup, name='register'),
+    path("signup/", views.signup, name='signup'),
     path("shop/", views.shop, name='shop'),
     path("predict/", views.predict, name='predict'),
     path("admin/", admin.site.urls),    
