1393cc43a71942e8af03f61c701934f873060d34
Vijay Varadarajan <vijayvaradarajan84@gmail.com>
2023-12-21 19:29:23 +0530

    predict fnc updated

--- vedassist/views.py
+++ vedassist/views.py
@@ -18,8 +18,6 @@ from .tokens import account_activation_token
 
 from .models import User, Medicine, Transaction
 
-from .predictor import model_predict
-
 # Create your views here.
 def index(request):
     return render(request, "vedassist/index.html")
@@ -169,6 +167,39 @@ def predict_view(request):
         })
         
     return render(request, "vedassist/predict.html")
+
+
+import pandas as pd
+import pickle
+import joblib
+import os
+
+
+
+def model_predict(user_input):
+    
+    classifier = joblib.load("vedassist/model.pkl")
+    
+    user_input = user_input.split(',')
+    user_data = pd.DataFrame({ 
+            'Cold': [user_input[0]],
+            'Eyepain' :[user_input[1]],
+            'Fever': [user_input[2]],
+            'Headache': [user_input[3]],
+            'Stomachache': [user_input[4]],
+            'Dizziness': [user_input[5]],
+            'Vomiting': [user_input[6]],
+            'Chestpain': [user_input[7]],
+            'Jointpain': [user_input[8]],
+            'Loosemotion': [user_input[9]],
+            'Throatinfection':[user_input[10]],
+            'Age': [user_input[11]],
+            'Gender': [user_input[12]],
+            'Weight': [user_input[13]],
+    })
+    prediction = classifier.predict(user_data)
+    return prediction
+
 
 
 def shop_view(request):
