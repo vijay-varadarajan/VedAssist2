1fed9cf4d64964f67b8365820e6ee3088d2705dd
Vijay Varadarajan <vijayvaradarajan84@gmail.com>
2023-12-27 20:27:10 +0530

    staticfiles debug

--- project/settings.py
+++ project/settings.py
@@ -29,10 +29,10 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS=['*']
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
+    "https://vedassist.onrender.com/",
 ]
 # CORS_ALLOW_ALL_ORIGINS=True
 
@@ -195,7 +195,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "staticfiles/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
